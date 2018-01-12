785ed1093563cfca5bdf7c16f14783476c4714e1
simulan <simulan88@yahoo.com>
2018-01-12 20:43:32 +0100

    render to texture feature, bufferservice & rendercontext classes

--- Core/Render/RenderToTexture/Programs/RTTFragmentShader.cs
+++ Core/Render/RenderToTexture/Programs/RTTFragmentShader.cs
@@ -20,4 +20,4 @@ namespace UMLProgram.Core.Render.RenderToTexture.Programs {
         ";
     }
 }
-//color = texture( renderedTexture, UV + 0.005*vec2( sin(time+1024.0*UV.x),cos(time+768.0*UV.y)) ).xyz ;
+
